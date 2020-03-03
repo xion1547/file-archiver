4113a0b43b4fc06af280ea7f05d4ab5623ad6576
xion1547 <vantoh2@gmail.com>
2020-03-03 16:47:28 -0600

    Creates starting source dir, and starting main file

---
+++ src/main/java/com/fileArchiver/main.java
@@ -0,0 +1,11 @@
+package com.fileArchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
