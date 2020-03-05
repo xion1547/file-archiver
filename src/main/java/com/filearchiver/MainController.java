1a892554c9224542d89b77d72738519985247228
xion1547 <vantoh2@gmail.com>
2020-03-05 03:18:39 -0600

    Adds controller with string output and test for that

---
+++ src/main/java/com/filearchiver/MainController.java
@@ -0,0 +1,14 @@
+package com.filearchiver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MainController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello from me";
+    }
+
+}
