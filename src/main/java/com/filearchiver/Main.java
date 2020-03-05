42e2e4831111b140092a1a8e5665f24b9f071c32
xion1547 <vantoh2@gmail.com>
2020-03-05 02:58:59 -0600

    Updates build.gradle and adds SpringBoot to Main.

--- src/main/java/com/filearchiver/Main.java
+++ src/main/java/com/filearchiver/Main.java
@@ -1,11 +1,11 @@
-package com.fileArchiver;
+package com.filearchiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class main {
+public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
