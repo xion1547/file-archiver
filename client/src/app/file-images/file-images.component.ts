84025d4f7e9af3ef60c19be593cd81c2ca7619bb
Vantou <xion1547@morris.umn.edu>
2021-09-02 15:33:36 -0500

    Adds component for FileImages   Allows us to generate S3 Presign urls and put it inside an array. Adds methods getIds to make a string of Ids from list of files Adds generateImages to fetch presignUrls Adds ngOnChanges to generate urls based on page number

---
+++ client/src/app/file-images/file-images.component.ts
@@ -0,0 +1,48 @@
+import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
+import {File} from "../files/file";
+import {FileService} from "../files/file.service";
+
+@Component({
+  selector: 'app-file-images',
+  templateUrl: './file-images.component.html',
+  styleUrls: ['./file-images.component.scss']
+})
+export class FileImagesComponent implements OnInit {
+
+  @Input() files?: File[];
+  @Input() pages?: number;
+
+  urls: string[] = [];
+
+  constructor(private fileService: FileService) { }
+
+  getIds(array: File[]): string{
+    let result: string = "";
+    for (let i = 0; i<array.length; i++) {
+      result = result + array[i].id.toString() +",";
+    }
+    return result;
+  }
+
+  generateImages(): void {
+    if (this.files) {
+      this.fileService.generateUrls(this.getIds(this.files))
+        .subscribe(files => {
+          this.urls = Object.values(files)
+        });
+    }
+  }
+
+  ngOnInit(): void {
+    //this.generateImages();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.pages) {
+      this.generateImages();
+    }
+    //this.generateImages();
+    console.log(changes)
+  }
+
+}
