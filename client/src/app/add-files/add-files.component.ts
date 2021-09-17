14f70138f89a2b9b7e2689a0062f681d0bd276ff
xion1547 <xion1547@morris.umn.edu>
2021-09-17 16:54:18 -0500

    Adds output variable to pass the newly added file to parent component

--- client/src/app/add-files/add-files.component.ts
+++ client/src/app/add-files/add-files.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FileService} from "../files/file.service";
+import {File} from "../files/file";
 
 @Component({
   selector: 'app-add-files',
@@ -7,6 +8,8 @@ import {FileService} from "../files/file.service";
   styleUrls: ['./add-files.component.scss']
 })
 export class AddFilesComponent implements OnInit {
+
+  @Output() addedFile: EventEmitter<any> = new EventEmitter<File>();
 
   show: boolean = false;
   selectedFile?: File;
@@ -26,7 +29,7 @@ export class AddFilesComponent implements OnInit {
     if (this.selectedFile == undefined) {
       console.log('no file selected');
     } else if (this.selectedFile) {
-      this.fileService.uploadToS3(this.selectedFile);
+      this.fileService.uploadToS3(this.selectedFile, this.addedFile);
     } else {
       console.log("Error happened");
     }
